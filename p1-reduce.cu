2435228223f828b4b1b45f8b8d97282db14af7cb
Arneal <arneal.montejo@outlook.com>
2025-12-19 03:22:24 -0600

    added max function utilizing cuda multi threading

--- p1-reduce.cu
+++ p1-reduce.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cuda_runtime.h>
+#include <algorithm>  
 using namespace std;
 
 #define BLOCK_SIZE 256
@@ -88,7 +89,7 @@ __global__ void sum_helper2(int * block_sums, int n){
 
 }
 
-int sum(int *h_arr, int arrSize){
+int cuda_sum(int *h_arr, int arrSize){
     int numThreads = 256;
     int size = arrSize * sizeof(int);
     int blocks = (arrSize + numThreads - 1)/numThreads;
@@ -113,8 +114,121 @@ int sum(int *h_arr, int arrSize){
 
     cudaFree(d_arr);
     cudaFree(d_blockSum);
+    free(h_blockSum);
 
     return h_blockSum[0];
+}
+
+__global__ void max_helper(int * arr, int n, int * maxes){
+    __shared__ int warp_vals[32];
+    int local_val = INT_MIN;
+
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for(int i = idx; i < n; i += stride){
+        local_val = max(local_val, arr[i]);
+    }
+
+    int lane = threadIdx.x %32;
+    int warp_id = threadIdx.x / 32;
+
+    for(int offset = 16; offset > 0; offset >>= 1){
+        local_val = max(local_val, __shfl_down_sync(0xffffffff, local_val, offset));
+    }
+
+    if(lane == 0){
+        warp_vals[warp_id] = local_val;
+    }
+
+    __syncthreads();
+
+    int block_sum = 0;
+    int warps_per_block = blockDim.x / warpSize;
+
+    if (warp_id == 0){
+        if(lane < warps_per_block){
+            block_sum = warp_vals[lane];
+        }else{
+            block_sum = INT_MIN;
+        }
+
+        for(int offset = 16; offset > 0; offset >>= 1 ){
+            block_sum = max(block_sum,
+                __shfl_down_sync(0xffffffff, block_sum, offset));
+        }
+
+        if(lane == 0){
+            maxes[blockIdx.x] = block_sum;
+        }
+    }
+}
+
+__global__ void max_helper2(int * block_maxes, int n){
+    __shared__ int warp_sums[32];
+
+    int tid = threadIdx.x;
+    int local_val = INT_MIN;
+
+    if(tid < n)
+        local_val = block_maxes[tid];
+
+    int lane = tid %32;
+    int warp_id = tid / 32;
+
+    for(int offset = 16; offset > 0; offset >>= 1){
+        local_val = max(local_val, __shfl_down_sync(0xffffffff, local_val, offset));
+    }
+
+    if(lane == 0){
+        warp_sums[warp_id] = local_val;
+    }
+
+    __syncthreads();
+
+    if(warp_id == 0){
+        local_val = (lane < (n + 31)/ 32) ? warp_sums[lane] : INT_MIN;
+
+        for(int offset = 16; offset > 0; offset >>=1){
+            local_val = max(local_val, __shfl_down_sync(0xffffffff, local_val, offset));
+        }
+
+        if(lane == 0){
+            block_maxes[0] = local_val;
+        }
+    }
+
+}
+
+int cuda_max(int * arr, int arrSize){
+    int min = INT_MIN;
+    int numThreads = 256;
+    int size = arrSize * sizeof(int);
+    int blocks = (arrSize + numThreads -1) / numThreads;
+    int blockSize = blocks * sizeof(int);
+
+    int *h_blockMaxes = (int*)malloc(blockSize);
+    int *d_arr, *d_blockMaxes;
+
+    cudaMalloc(&d_arr, size);
+    cudaMalloc(&d_blockMaxes, blockSize);
+
+    cudaMemcpy(d_arr, arr, size, cudaMemcpyHostToDevice);
+    
+    max_helper<<<blocks, numThreads>>>(d_arr, arrSize, d_blockMaxes);
+
+    cudaDeviceSynchronize();
+
+    max_helper2<<<1, numThreads>>>(d_blockMaxes, blocks);
+
+    cudaMemcpy(h_blockMaxes, d_blockMaxes, blockSize, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_arr);
+    cudaFree(d_blockMaxes);
+    free(h_blockMaxes);
+
+    return h_blockMaxes[0];
+
 }
 
 
